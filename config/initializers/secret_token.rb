1330babdf182c9a8682be6c21b2069f0c65d4a95
Manuel Salinas <m_salinas@hotmail.es>
2014-03-13 23:45:45 +0100

    Initial commit_Semana_1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportsShopOne::Application.config.secret_key_base = '4463d4aea0b1ea52442f1763f8058903280a061c5899d54f0eb8be1fa455c4aa18627691f67133a3013bc4a151492796a10339d000e2429ffd1a2034ea86fc8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SportsShopOne::Application.config.session_store :cookie_store, key: '_sports_shop_one_session'
