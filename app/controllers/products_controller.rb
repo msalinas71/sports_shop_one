55878ed6e6b01f2e257a05b7d9d36fb68a0849b4
Manuel Salinas <m_salinas@hotmail.es>
2014-03-25 16:46:27 +0100

    Commit_semana_4_View_aa

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :registered_at, :manufacturer, :stock)
+      params.require(:product).permit(:name, :description, :registered_at, :manufacturer, :stock, :product_category)
     end
 end
